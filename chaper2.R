393ebfb34e395c9592a35fb278c60c1ca821ab2f
SEONZONE <48579953+SEONZONE@users.noreply.github.com>
2020-01-17 17:29:20 +0900

    2020/01/07 교재 공부

---
+++ chaper2.R
@@ -0,0 +1,42 @@
+x <- 10
+x
+
+
+x<-c(10,20,30)
+x
+
+
+x<-c(10:15)
+x
+
+
+x<-seq(10,15)
+x  
+
+
+x<-seq(1,10,by=4)
+x
+
+x<-"HI"
+x
+
+
+x<-c("HEllo","Hi","Hello~!!!!")
+x
+
+ex_vector1 <- c(-1,0,1)
+ex_vector1
+
+
+str(ex_vector1) # 변수의 데이터 구조를 알려준다. 
+length(ex_vector1) # 벡터의 개수 즉, 길이를 말한다. 
+
+ex_vector2 <- c("1","2","3")
+ex_vector2
+
+str(ex_vector2)
+
+ex_vector3 <- c(TRUE,FALSE,TRUE,FALSE)
+ex_vector3
+
+str(ex_vector3)
