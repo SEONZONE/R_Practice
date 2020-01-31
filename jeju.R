75986427c84ee16a95ee82f18c629cbefeb70dac
seonzone <tjdwhd2247@gmail.com>
2020-01-31 15:45:42 +0900

    wordcloud 작업

--- jeju.R
+++ jeju.R
@@ -1,4 +1,4 @@
-
+install.packages("rJava")
 library(rJava)
 library(KoNLP)
 library(wordcloud2)
@@ -7,8 +7,9 @@ useSystemDic()
 useSejongDic()
 useNIADic()
 
-word_data <- readLines("제주도여행지추천.txt",encoding="UTF-8")
-word_data <- gsub("\\d","",word_data) # D 는 숫자를 제외한 문자 , 특수문자
+word_data <- readLines("txt/제주도여행지추천.txt",encoding="UTF-8")
+word_data <- gsub("\\d","",word_data)
+# D 는 숫자를 제외한 문자 , 특수문자
 #word_data <- gsub("\\W","",word_data)  # 대문자 W 는 특수기호 암호화 처리 
 word_data <- sapply(word_data,extractNoun,USE.NAMES = F)
 undata <- unlist(word_data)
